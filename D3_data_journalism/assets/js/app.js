ce92e2daeebd6c8da6187ca3c7301c47b4bfa8de
JoseH-Git <joseh.sierra@outlook.com>
2021-07-09 13:40:21 -0400

    Smoke and Age Scatter

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,43 +1,101 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 500;
 
-// append the svg object to the body of the page
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Select body, append SVG area to it, and set its dimensions
 var svg = d3.select("#scatter")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-//Read the data
-d3.csv("assets/data/data.csv", function(data) {
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 30])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+// Append a group area, then set its margins
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 30])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+// // Configure a parseTime function which will return a new Date object from a string
+// var parseTime = d3.timeParse("%B");
 
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(data.smokes); } )
-      .attr("cy", function (d) { return y(data.age); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+// Load data from miles-walked-this-month.csv
+d3.csv("assets/data/data.csv").then(function(data) {
 
-})
+  // Print the milesData
+  console.log(data);
+
+  // Format the date and cast the miles value to a number
+  data.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+    // console.log(data.age);
+    // console.log(data.smokes);
+  });
+
+  // Configure a time scale with a range between 0 and the chartWidth
+  // Set the domain for the xTimeScale function
+  // d3.extent returns the an array containing the min and max values for the property specified
+  var xTimeScale = d3.scaleTime()
+    .range([0, chartWidth])
+    .domain(d3.extent(data, data => data.age));
+
+  // Configure a linear scale with a range between the chartHeight and 0
+  // Set the domain for the xLinearScale function
+  var yLinearScale = d3.scaleLinear()
+    .range([chartHeight, 0])
+    .domain([0, d3.max(data, data => data.smokes)]);
+
+  // Create two new functions passing the scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xTimeScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // // Configure a drawLine function which will use our scales to plot the line's points
+  // var drawLine = d3
+  //   .line()
+  //   .x(data => xTimeScale(data.age))
+  //   .y(data => yLinearScale(data.smokes));
+
+    
+  // append circles
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xTimeScale(d.age))
+  .attr("cy", d => yLinearScale(d.smokes))
+  .attr("r", "10")
+  .attr("fill", "red ")
+  .attr("stroke-width", ".5")
+  .attr("stroke", "blue");
+
+    
+  // // Append an SVG path and plot its points using the line function
+  // chartGroup.append("path")
+  //   // The drawLine function returns the instructions for creating the line for milesData
+  //   .attr("d", drawLine(data))
+  //   .classed("circle", true);
+
+  // Append an SVG group element to the SVG area, create the left axis inside of it
+  chartGroup.append("g")
+    .classed("axis", true)
+    .call(leftAxis);
+
+  // Append an SVG group element to the SVG area, create the bottom axis inside of it
+  // Translate the bottom axis to the bottom of the page
+  chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", "translate(0, " + chartHeight + ")")
+    .call(bottomAxis);
+}).catch(function(error) {
+  console.log(error);
+});
